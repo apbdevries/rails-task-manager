e178d6436d0cbc853f7fe8739c918df008f228c9
Adam de Vries <apbdevries@gmail.com>
2020-11-24 19:23:08 +0800

    done

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,11 @@
+class TasksController < ApplicationController
+    def index
+        @task = Task.all
+    end
+    def show
+        @task = Task.find(params[:id])
+    end
+    def new
+        @task = Task.new
+    end
+end
